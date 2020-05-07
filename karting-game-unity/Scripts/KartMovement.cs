b9847b0dadfa151ce4768f7f02d58c2b0e95fee4
ryanlambie <42672777+ryanlambie@users.noreply.github.com>
2020-05-07 07:43:52 +0100

    Add files via upload

---
+++ karting-game-unity/Scripts/KartMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KartMovement : MonoBehaviour
+{
+    private float speed;
+    private float drag = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0;
+    }
+    private void FixedUpdate()
+     {
+         transform.Translate(speed * Time.deltaTime, 0, 0, Space.Self);
+     }
+    // Update is called once per frame
+    void Update()
+    {
+        float rot = Input.GetAxis("Horizontal") * 10;
+        gameObject.transform.Rotate(0.0f, 0.0f, rot* speed * Time.deltaTime, Space.Self);
+        float acc = Input.GetAxis("Vertical") * 10;
+        speed += acc * Time.deltaTime;
+        if(speed > 18){
+            speed = 18;
+        }
+        if(speed > 0){
+            speed -= drag * Time.deltaTime;
+        }
+        if(speed < 0){
+            speed += drag * Time.deltaTime;
+        }
+    }
+}
---
+++ karting-game-unity/Scripts/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public Text timeCounter;
+    static float timer; 
+    public Text LastLapText;
+    public GameObject RestartButton;
+
+    void Start(){
+        RestartButton.SetActive(false);
+    }
+   
+    public void restartGame(){
+        Debug.Log("restart");
+        RestartButton.SetActive(false);
+        SceneManager.LoadScene("KartingScene");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(timer / 60F);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+        string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+        timeCounter.text = niceTime;
+
+        if(GameObject.Find("Kart").transform.position.y < 0){
+            RestartButton.SetActive(true);
+        }
+
+        if(Input.GetKeyDown("joystick button 9")){
+            restartGame();
+        }
+    }
+
+    void OnTriggerExit(Collider other){
+        LastLapText.text = "Last Lap: "+timeCounter.text;
+        timer = 0.0f;
+    }
+}
